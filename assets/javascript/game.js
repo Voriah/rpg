b4ed2401848c1149adda9bba422d3dfc4de06584
Voriah <rodgick@yahoo.com>
2018-08-18 14:05:37 -0700

    vfdk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,18 @@
+function right () { 
+  $('body').animate({
+    'background-position-x': '+=50px',
+  });
+}
+
+function left () { 
+  $('body').animate({
+    'background-position-x': '-=50px',
+  });
+}
+
+var audio = new Audio("assets/audio/castle.mp3");
+audio.play();
+
 document.onkeypress = function (evt) {
   evt = evt || window.event;
   var charCode = evt.keyCode || evt.which;
@@ -11,15 +26,18 @@ document.onkeypress = function (evt) {
       break;
     case 100: 
       $("#p").attr("src", "assets/images/walkyg.gif");
+      left();
       break;
     case 97: 
       $("#p").attr("src", "assets/images/walkyback.gif");
+      right();
       break;
     case 102: 
       $("#p").attr("src", "assets/images/atackf.gif");
       break;
 
   }
+
   // if (input === 32) {
   //   $("#p").attr("src", "assets/images/jump.gif");
   // }
