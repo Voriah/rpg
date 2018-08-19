9c371fe8e68290d9dde9da6d4da94cfecf9c87d9
Voriah <rodgick@yahoo.com>
2018-08-18 17:59:07 -0700

    lifebars wooo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,28 @@
+var playerChar = {
+  name: "Richter",
+  dam: 45,
+  hp: 100,
+  exp: 0
+}
+var enemyChar = {
+  name: "Dracula",
+  dam: 50,
+  hp: 150,
+}
+
+
 function right () { 
   $('body').animate({
     'background-position-x': '+=50px',
-  });
+  }, 300);
 }
 
 function left () { 
   $('body').animate({
     'background-position-x': '-=50px',
-  });
+  }, 300);
 }
 
-var audio = new Audio("assets/audio/castle.mp3");
-audio.play();
 
 document.onkeypress = function (evt) {
   evt = evt || window.event;
@@ -23,6 +34,7 @@ document.onkeypress = function (evt) {
   switch (input) {
     case 32: 
       $("#p").attr("src", "assets/images/jump.gif");
+      enemyDamage();
       break;
     case 100: 
       $("#p").attr("src", "assets/images/walkyg.gif");
@@ -34,9 +46,46 @@ document.onkeypress = function (evt) {
       break;
     case 102: 
       $("#p").attr("src", "assets/images/atackf.gif");
+      playerDamage();
       break;
 
   }
+
+  
+  function playerDamage() {
+    var p = enemyChar.hp/10;
+    var x = playerChar.dam;
+    var d = 20;
+    for (let i = 20; i > 0; i--) {
+      if ($(`#ehp${i}`).css("color") !== "rgb(255, 0, 0)"){
+        d--;
+      }
+    }
+    for (x; x >0; x-=p) {
+      if (x >= p) {
+      $(`#ehp${d}`).css("color", "white")
+      d--;
+      }
+      }
+}
+
+  function enemyDamage() {
+    var p = playerChar.hp / 10;
+    var x = enemyChar.dam;
+    var d = 20;
+    for (let i = 20; i > 0; i--) {
+      if ($(`#hp${i}`).css("color") !== "rgb(255, 0, 0)"){
+        d--;
+      }
+    }
+    for (x; x >0; x-=p) {
+      if (x >= p) {
+      $(`#hp${d}`).css("color", "white")
+      d--;
+      }
+    }
+}
+
 
   // if (input === 32) {
   //   $("#p").attr("src", "assets/images/jump.gif");
